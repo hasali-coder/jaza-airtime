cd27e2a42b9cde0696d1562fc6fdad2d398fa9ea
hasali-coder <58490554+hasali-coder@users.noreply.github.com>
2021-07-18 15:20:41 +0300

    Update templatemo-script.js

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,5 +1,5 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['2.jpg', '3.jpg', '4.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
